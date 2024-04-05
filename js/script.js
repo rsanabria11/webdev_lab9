a97c685ad2d4af1e39ed3a8cdd45dc8199cb3755
rsanabria11 <rsanabria@hawk.iit.edu>
2024-04-04 22:22:45 -0500

    adding files and some javascript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function init(){
+//add your javascrip between these two lines of code
+	var button = document.getElemenetId('entrybutton');
+
+	function showMeText(){
+		var
+	}
+
+
+
+
+
+
+window.addEventListener('load', init);
