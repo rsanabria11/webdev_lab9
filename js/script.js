ea9354cad1098aaed01146d2c87d77ce608e8e42
rsanabria11 <rsanabria@hawk.iit.edu>
2024-04-05 22:12:46 -0500

    finding out why it wont work- testing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+
 function init(){
-	var button = document.getElementById('entrybutton');
+  var button = document.getElementById('entrybutton');
 
-	function showMeText(){
-		var textbox = document.getElementById('entryinput');
-		document.getElementById('textoutput').innerHTML = textbox.value;
-		alert("Rita Sanabria: " + textbox.value);
-	} 
-	button.addEventListener('click', showMeText);
-}
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Rita Sanabria: " + textbox.value);
+  }
 
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
